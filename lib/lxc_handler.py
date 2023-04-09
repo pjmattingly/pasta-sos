b949d5e7e7781d7c9e9133b7ebbaf1380245a2e9
Peter Mattingly <peter.mattingly@canonical.com>
2023-04-09 17:38:34 -0600

    added helper method to check if lxc is installed

--- lib/lxc_handler.py
+++ lib/lxc_handler.py
@@ -3,3 +3,11 @@ check if lxc/lxd is installed
 
 then, given a compressed rootfs, create a container from it
 '''
+
+import shutil
+
+def is_installed():
+    '''
+    Check if debootstrap is installed.
+    '''
+    return bool(shutil.which('lxc'))
