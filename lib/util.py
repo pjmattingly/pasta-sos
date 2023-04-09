3dcc32c721c00e33462b91531de932af99e4358a
Peter Mattingly <peter.mattingly@canonical.com>
2023-04-09 17:58:32 -0600

    made a function to write a metadata.yaml file for use with lxc

--- lib/util.py
+++ lib/util.py
@@ -6,6 +6,7 @@ Compress a directory and return a path to the produced file
 import tarfile
 from pathlib import Path
 import os
+import time
 
 class Bad_Input(Exception): pass
 class Bad_Target(Exception): pass
@@ -19,7 +20,7 @@ def tar_gzip(source_dir, output_dir):
     _out = Path(output_dir)
 
     if not ( _in.exists() and _in.is_dir() ): raise Bad_Input(f"Could not compress '{_in.resolve()}' into a rootfs archive.")
-    if not ( _out.exists() and _in.is_dir() and os.access(_out, os.W_OK | os.X_OK) ): raise Bad_Target(f"Cannot write rootfs to '{_out.resolve()}'.")
+    if not ( _out.exists() and _out.is_dir() and os.access(_out, os.W_OK | os.X_OK) ): raise Bad_Target(f"Cannot write to '{_out.resolve()}'.")
 
     _out = _out / "rootfs.tar.gz"
 
@@ -29,3 +30,28 @@ def tar_gzip(source_dir, output_dir):
         tar.add(_in, arcname='/')
 
     return str(_out.resolve())
+
+def make_metadata_yaml(distro, output_dir):
+    '''
+    Create a metadata.yaml file for lxc, and return the path to the file
+    '''
+
+    _out = Path(output_dir)
+    if not ( _out.exists() and _out.is_dir() and os.access(_out, os.W_OK | os.X_OK) ): raise Bad_Target(f"Cannot write to '{_out.resolve()}'.")
+    
+    _content = f'''
+    architecture: "x86_64"
+    creation_date: {time.time()}
+    properties:
+    architecture: "x86_64"
+    description: "Automatically generated install of '{distro}' by pasta-sos."
+    os: "ubuntu"
+    release: "{distro}"
+    '''
+
+    _cleaned_content = "\n".join([line.strip() for line in _content.strip().split("\n")])
+    
+    _out_file = _out / 'metadata.yaml'
+    _out_file.write_text(_cleaned_content)
+
+    return(_out_file)
