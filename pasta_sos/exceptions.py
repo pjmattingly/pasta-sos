a6cfafb5f9fda3ac2aca304057f257f5c9dd14ed
Peter Mattingly <peter.mattingly@canonical.com>
2023-08-05 14:30:00 -0600

    starting fresh for v2

--- pasta_sos/exceptions.py
+++
@@ -1,9 +0,0 @@
-class SosreportNotFound(Exception): pass
-class SosreportUnreadable(Exception): pass
-class NotSosreport(Exception): pass
-class DebootstrapNotInstalled(Exception): pass
-class LxcNotInstalled(Exception): pass
-class HotsosNotInstalled(Exception): pass
-
-
-
