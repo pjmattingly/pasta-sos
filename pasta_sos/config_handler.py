338c59c16ee16c4150b8ac03c0c264d2768255e9
Peter Mattingly <peter.mattingly@canonical.com>
2023-08-06 20:30:28 -0600

    1

--- pasta_sos/config_handler.py
+++ pasta_sos/config_handler.py
@@ -1,5 +1,12 @@
 from pathlib import Path
-_default_loc = Path.home() / ".pasta-sos" / 'config.json'
+_default_dir = Path.home() / ".pasta-sos"
+_default_conf = _default_dir / 'config.json'
 
 def exists():
-    return _default_loc.exists()
+    return _conf_exists()
+
+def _dir_exists():
+    return _default_dir.exists()
+
+def _conf_exists():
+    return _default_conf.exists()
