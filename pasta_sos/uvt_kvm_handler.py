9eb9e55665c10c688d19cb12e7be8e099cc192a0
Peter Mattingly <peter.mattingly@canonical.com>
2023-08-06 22:06:28 -0600

    1

--- pasta_sos/uvt_kvm_handler.py
+++ pasta_sos/uvt_kvm_handler.py
@@ -1,4 +1,10 @@
 import shutil
 
+class UvtNotInstalled(Exception):
+    def __init__(self):
+        m = "Please install 'uvtool': `sudo apt -y install uvtool`."
+        
+        super().__init__(m)
+
 def is_installed():
     return bool(shutil.which('uvt-kvm') and shutil.which('uvt-simplestreams-libvirt'))
