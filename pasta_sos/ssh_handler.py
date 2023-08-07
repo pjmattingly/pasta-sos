88b6a0b69c3473b9bbfcab7d917ff59e505e9630
Peter Mattingly <peter.mattingly@canonical.com>
2023-08-06 22:57:04 -0600

    1

--- pasta_sos/ssh_handler.py
+++ pasta_sos/ssh_handler.py
@@ -1,29 +1,16 @@
 import os
 import psutil
+from pathlib import Path
 
-class SSHAgentNotReady(Exception):
-    def __init__(self):
-        m = "The process `ssh-agent` does not seem to be running or setup correctly."
-        m += "\n"
-        m += "Please run: 'eval `ssh-agent`' to launch."
-        m += "\n"
-        m += "https://manpages.ubuntu.com/manpages/ssh-agent.1.html"
-        
-        super().__init__(m)
+def is_key_on_system(key):
+    _key = str(key).strip()
 
-def _agent_running():
-    '''
-    To check that public keys are on the system, we need `ssh-agent` running and
-    correctly configured
-    To correctly configure `ssh-agent` is sets two environment variables 
-    ("SSH_AGENT_PID" and "SSH_AUTH_SOCK")
-    and "SSH_AGENT_PID" contains its PID
-    and so checking if its PID is present shows if the process is running
-        see:
-        https://manpages.ubuntu.com/manpages/jammy/man1/ssh-agent.1.html
-    '''
-    if ("SSH_AGENT_PID" in os.environ and "SSH_AUTH_SOCK" in os.environ):
-        return psutil.pid_exists(os.environ["SSH_AGENT_PID"])
+    for pub in _get_public_keys():
+        _local_key = Path(pub).read_text().strip()
 
-def ready():
-    return _agent_running()
+        if _key == _local_key:
+            return True
+    return False 
+
+def _get_public_keys():
+    return [file for file in (Path.home() / ".ssh").glob('**/*.pub')]
