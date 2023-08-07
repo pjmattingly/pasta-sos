5d059b6a2f874b786e38a995bf5af91e740dd886
Peter Mattingly <peter.mattingly@canonical.com>
2023-08-06 21:23:28 -0600

    1

---
+++ pasta_sos/launchpad_handler.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+def username_exists(username):
+    try:
+        with urlopen(f"https://launchpad.net/~{username}") as response:
+            soup = BeautifulSoup(response, 'html.parser')
+            
+            #check for unknown user, but with existing Launchpad page
+            #e.g., https://launchpad.net/~test
+            #see: https://en.wikipedia.org/wiki/Beautiful_Soup_(HTML_parser)
+            #and: https://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start
+            if "does not use Launchpad" in soup.get_text():
+                
+                return False
+    except HTTPError:
+        #check for unknown user (404); e.g., `some-fake-name`
+        #or other errors response.status >= 400
+        return False
+
+    return True
