5db22569798b46ab5f2cf5895243ea928dd5647b
Peter Mattingly <peter.mattingly@canonical.com>
2023-08-06 20:46:29 -0600

    1

--- pasta_sos/config_handler.py
+++ pasta_sos/config_handler.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+import shelve
+
 _default_dir = Path.home() / ".pasta-sos"
-_default_conf = _default_dir / 'config.json'
+_defalt_name = 'config'
+_default_conf = _default_dir / _defalt_name
 
 def exists():
     return _conf_exists()
@@ -10,3 +13,19 @@ def _dir_exists():
 
 def _conf_exists():
     return _default_conf.exists()
+
+def setup():
+    if not _dir_exists():
+        _default_dir.mkdir()
+    
+    #if not _conf_exists():
+    #    _default_conf.touch()
+
+def add(key, value):
+    return _add(key, value)
+
+def _add(key, value):
+    pass
+
+def add_LaunchPad_username(value):
+    pass
